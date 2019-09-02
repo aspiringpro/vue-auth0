a9b5d1cc707cba5ed7d06ed4e44f59ee3600afda
MarcusReed <marcusjamesreed@gmail.com>
2019-09-02 14:00:58 -0700

    made the AppBar a component

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar app>
-
-      <v-toolbar-title class="headline text-uppercase">
-        <span>VUE + </span>
-        <span class="font-weight-light">Auth0</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn to="/" class="mr-2">Home</v-btn>
-      <v-btn to="/about" class="mr-2">About</v-btn>
-      <v-btn to="/contact" class="mr-2">Contact</v-btn>
-      <v-btn to="/members" class="mr-2">Members Only</v-btn>
-      <v-btn class="mr-2" @click="login" v-if="!this.$store.state.userIsAuthorized" color="primary">Signup/Login</v-btn>
-      <v-btn class="mr-2" @click="logout" v-if="this.$store.state.userIsAuthorized">Logout</v-btn>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-    </v-app-bar>
+    <AppBar/>
 
     <v-content>
       <router-view/>
@@ -24,12 +10,12 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import AppBar from './components/AppBar';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    AppBar,
   },
   data: () => {
     return {
